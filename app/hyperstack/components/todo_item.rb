a34c9977439b3272c83e3c9cd81fddfe0ccd59ca
Mitch VanDuyn <mitch@catprint.com>
2019-04-06 05:21:24 +0000

    update gitpod file

--- app/hyperstack/components/todo_item.rb
+++ app/hyperstack/components/todo_item.rb
@@ -2,6 +2,7 @@ class TodoItem < HyperComponent
   param :item
   fires :delete_item
   render(DIV, class: 'ToDoItem') do
+    P { 'xx' }
     P(class: 'ToDoItem-Text') { @Item }
     BUTTON(class: 'ToDoItem-Delete'){ '-' }.on(:click) { delete_item! }
   end
